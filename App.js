cdedb4ae80d5674abbca74530094f335cfcb690a
Amit Suman <amit.suman456@gmail.com>
2024-01-03 01:39:45 +0530

    episode 3

--- App.js
+++ App.js
@@ -1,20 +1,28 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-const parent = React.createElement("div", { id: "parent" }, [
-    React.createElement("div", { id: "child" }, [
-      React.createElement("h1", {}, "I'm an h1 tag,this is  React"),
-      React.createElement("h2", {}, "I'm an h2 tag"),
-    ]),
-    React.createElement("div", { id: "child2" }, [
-      React.createElement("h1", {}, "I'm an h1 tag"),
-      React.createElement("h2", {}, "I'm an h2 tag"),
-    ]),
-  ]);
-  
-  //JSX
-  
-  console.log(parent); // object
-  
-  const root = ReactDOM.createRoot(document.getElementById("root"));
-  
-  root.render(parent);
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+// React Element => Object => HTML element once it render (root.render)
+
+const heading = React.createElement("h1", { id: "heading" }, "Namaste React"); //object //react elemt
+
+const jsxHeading = <h1 id="heading">Namaste React using JSX</h1>; //this is not HTML
+//class attribute is used in html but className is used in JSX
+//camelCase is used for attributes in JSX
+
+//React Component
+const Title = () => <h1>Namaste React using Javascript</h1>; //functional component
+const HeadingComponent = () => (
+  <div>
+    <Title />
+    {Title()}
+    <Title></Title>
+    {heading} react element inside component using curly braces
+    <h1>Namaste React Functional Component</h1>
+  </div>
+); //functional component
+// we can call the Title function as well and it will behave totally same {Title()}
+console.log(jsxHeading);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+// root.render(jsxHeading);
+root.render(<HeadingComponent />);
--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Namaste React 🚀
+# class attribute is used in html but className is used in JSX
+# camelCase is used for attributes in JSX
+# // React Element => Object => HTML element once it render (root.render)
