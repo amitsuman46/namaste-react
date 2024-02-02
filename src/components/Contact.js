4516b96a89d88790430e36eafe58b63c938fd9ca
Suman <amit.suman@accenture.com>
2024-02-02 17:22:56 +0530

    Ep13 pt4

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -3,7 +3,12 @@ import React from 'react'
 const Contact = () => {
   return (
     <div>
-        <h1>Contus us Page</h1>
+      <h1 className="font-bold text-2xl p-4 m-4">Contus us Page</h1>
+      <form>
+        <input type="text" className='border border-black p-2 m-2' placeholder='name' />
+        <input type="text" className='border border-black p-2 m-2' placeholder='message' />
+        <button className='border border-black p-2 m-2 bg-gray-200 rounded-lg'>Submit</button>
+      </form>
     </div>
   )
 }
